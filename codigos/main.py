e9a08f7c8f23647daf91c50c509ce7394db15a56
f3pe <fel234bf4@gmail.com>
2024-04-29 21:13:19 -0300

    Melhorando a interação com o programa

--- codigos/main.py
+++ codigos/main.py
@@ -31,33 +31,48 @@ def main():
     estado_inicial = (3,2,1,1)
     estado_final = (0,0,0,0)
 
-    resultado = search.Problem((3,2,1,1), (0,0,0,0), graph)
+    resultado = search.Problem(estado_inicial, estado_final, graph)
 
     busca_em_largura = search.breadth_first_graph_search(resultado)
     busca_em_profundidade = search.depth_first_graph_search(resultado)
     busca_em_profundidade_limitada = search.depth_limited_search(resultado, 20)
     busca_em_profundidade_limitada_iterativa = search.iterative_deepening_search(resultado)
 
+    
+    
+    continua = "sim"
+    while continua in "sim":
+        op = str(input("Digite o tipo de busca que você deja ver\n[1] - Busca em largura\n[2] - Busca em profundidade\n[3] - Busca em profundidade limitada\n[4] - Busca em profundidade limitada iterativa: "))
+        if op == '1':
+            solucao = busca_em_largura.solution()
+            print("Resultado da busca em largura:\n0 º ",estado_inicial)
+            for i, v in enumerate(solucao):
+                print(i+1,"º ", v)
+            print("\n\n")
+        elif op == '2':
+            solucao = busca_em_profundidade.solution()
+            print("Resultado da busca em profundidade:\n0 º ",estado_inicial)
+            for i, v in enumerate(solucao):
+                print(i+1,"º ", v)
+            print("\n\n")
+        elif op == '3':
+            solucao = busca_em_profundidade_limitada.solution()
+            print("Resultado da busca em profundidade limitada:\n0 º ",estado_inicial)
+            for i, v in enumerate(solucao):
+                print(i+1,"º ", v)
+            print("\n\n")
+        elif op == '4':
+            solucao = busca_em_profundidade_limitada_iterativa.solution()
+            print("Resultado da busca em profundidade limitada e iterativa:\n0 º ",estado_inicial)
+            for i, v in enumerate(solucao):
+                print(i+1,"º ", v)
+            print("\n\n")
+        else:
+            print("Escolha uma opção valida por favor \n\n")
 
-    solucao = busca_em_largura.solution()
-    print("Resultado da busca em largura:\n0 º ",estado_inicial)
-    for i, v in enumerate(solucao):
-        print(i+1,"º ", v)
-    print("\n\n")
-    solucao = busca_em_profundidade.solution()
-    print("Resultado da busca em profundidade:\n0 º ",estado_inicial)
-    for i, v in enumerate(solucao):
-        print(i+1,"º ", v)
-    print("\n\n")
-    solucao = busca_em_profundidade_limitada.solution()
-    print("Resultado da busca em profundidade limitada:\n0 º ",estado_inicial)
-    for i, v in enumerate(solucao):
-        print(i+1,"º ", v)
-    print("\n\n")
-    solucao = busca_em_profundidade_limitada_iterativa.solution()
-    print("Resultado da busca em profundidade limitada e iterativa:\n0 º ",estado_inicial)
-    for i, v in enumerate(solucao):
-        print(i+1,"º ", v)
+        continua = str(input("Deseja continuar?[S/N]")).lower()
+    print("Programa Finalizado")
+    
 
 if __name__=='__main__':
     main()
